50c2f452885d035904493c0263af439701e738a7
marionboynton <43375834+marionboynton@users.noreply.github.com>
2022-12-12 13:26:34 +0100

    Remove exploration steps
    removing added exploratory steps as the increased entropy regularization
    enables the agent to explore sufficently and learn to reach the target. The
    exploratory steps do not appear to improve th eperformance beyond this.
    
    Average episode rewards over 100 episodes running the deterministic policy for
    the trained model without the exploratory steps is:
    
    avg: 0.9972813845104688 max: 1.0000000000000002 min: 0.7281384510468834
    
    and with the exploratory steps is:
    
    avg: 0.991840809232942 max: 1.0000000000000002 min: 0.7280179918923811

--- train_and_test_hppo.py
+++ train_and_test_hppo.py
@@ -35,10 +35,6 @@ action_std_decay_freq = 10000  # action_std decay frequency (in num timesteps)
 max_episodes = 10000
 update_freq = 64
 
-explore_step_freq = 20
-#explore_episode_freq = 10
-
-
 
 device = torch.device('cpu')
 if(torch.cuda.is_available()): 
@@ -50,7 +46,6 @@ else:
 
 
 def convert_action(act_idx, act_param):
-    #print(act_param[act_idx])
     params = [np.zeros((1,), dtype=np.float32), np.zeros((1,), dtype=np.float32), np.zeros((1,), dtype=np.float32)]
     params[act_idx][:] = PARAMETERS_MAX[act_idx] * (act_param + 1)/2
     return (act_idx, params)
@@ -64,59 +59,26 @@ def train(env, agent):
 
     total_reward = 0
     returns = []
-    returns_avg_20 = [] # average of last 20 episodes' total rewards
-    returns_avg_100 = []
     eval_returns = [] # running detemrinistic policy to evaluate
     
     max_episode_len = 30
     num_episodes = 0
     
     step = 0
-    
-    #recent_steps_100 = []
-    #step_avg_100 = 0
-    r_avg_100 = 0
 
     discrete_losses = [] # for plotting losses
     cont_losses = []
 
-    explore_step_freq_cont = 8 # take exploratory steps
-    explore_step_freq_disc = 10
-    #print(explore_step_freq)
-    explore_500 = False # to encourage exploration from where the agent fails to step at eval_steps_avg - 1
-    eval_steps_avg = 0
-
     for _ in range(max_episodes):
-
         state, _ = env.reset()
         state = np.array(reduce_state(state), dtype=np.float32, copy=False)
 
         episode_reward = 0
         episode_steps = 0
-        exploring = False
-        
-        if explore_500: # or num_episodes % explore_episode_freq == 0:
-            exploring = True
-        
 
         for _ in range(max_episode_len):
-            explore_cont = False
-            explore_discrete = False
-            
-            #if (exploring and episode_steps > step_avg_100) or step % explore_step_freq == 0:
-            if (exploring and (episode_steps >= (eval_steps_avg - 1))): #episode_reward > r_avg_100): #and ):
-                explore_cont = True
-  
-                #if exploring and episode_reward > r_avg_100:
-                #    print("exploring")
-
-            if step % explore_step_freq_cont == 0:
-                explore_cont = True
-            if step % explore_step_freq_disc == 0:
-                explore_discrete = True
-
-            cont_action, cont_log_prob = agent.get_continuous_action(state, explore=explore_cont) # (1, 3)
-            discrete_action, discrete_log_prob = agent.get_discrete_action(state, explore=explore_discrete)
+            cont_action, cont_log_prob = agent.get_continuous_action(state) # (1, 3)
+            discrete_action, discrete_log_prob = agent.get_discrete_action(state)
 
             action = convert_action(discrete_action.item(), np.array(cont_action, dtype=np.float32, copy=False)[0])
 
@@ -146,43 +108,21 @@ def train(env, agent):
             
             if terminal:
                 break
-        
-        #recent_steps_100.append(episode_steps)
+
         num_episodes += 1
         returns.append(episode_reward)
         total_reward += episode_reward
 
-        if num_episodes % 20 == 0:
-            returns_avg_20.append([np.mean(returns[-20:]), stats.sem(returns[-20:])])
-
         if num_episodes % 100 == 0:
-            returns_avg_100.append(np.mean(returns[-100:]))
-            #step_avg_100 = int(np.mean(recent_steps_100))
-            #step_avg_100 = int(np.percentile(recent_steps_100, 80))
-            r_avg_100 = np.percentile(returns[-100:], 60)
-            #recent_steps_100 = []
-            print(f'episode {num_episodes} episode reward: {episode_reward} r100: {returns_avg_100[-1]} explore reward: {r_avg_100} continuous action std dev: {agent.action_std}') #
-
-            
-            explore_500 = False
-        
-        #if explore_500 and num_episodes % 200 == 0:
-        #    explore_500 = False
+            print(f'episode {num_episodes} episode reward: {episode_reward} r100: {np.mean(returns[-100:])} explore reward: {r_avg_100} continuous action std dev: {agent.action_std}') #
 
         if num_episodes % 500 == 0:
             evals = []
-            eval_steps_avg = 0
             for _ in range(10):
-                r, n_steps = test(env, agent, deterministic=True)
+                r = test(env, agent, deterministic=True)
                 evals.append(r)
-                eval_steps_avg += n_steps
             eval_returns.append([np.mean(evals), max(evals), min(evals)])
-            eval_steps_avg //= 10
 
-            #explore_step_freq = min(explore_step_freq + 10, 100)
-            #explore_episode_freq = max(explore_episode_freq - 1, 2)
-
-            explore_500 = True
 
     end_time = time.time()
     print("Took %.2f seconds" % (end_time - start_time))
@@ -222,13 +162,13 @@ def test(env, agent, deterministic=True):
         state = np.array(reduce_state(next_state), dtype=np.float32, copy=False)
 
         ##env.render()
-        #time.sleep(0.002)
+        #time.sleep(0.02)
         
         if terminal:
             break
     
     print(f'steps: {step} episode reward: {episode_reward}')
-    return episode_reward, step
+    return episode_reward
 
     
 def main():
@@ -253,22 +193,25 @@ def main():
     else:
         train(env, agent)
 
-    agent.action_std = 0.1
-    
-    # stochastic policy test
-    print("stochastic policy test")
-    
     evals = []
-    for _ in range(10):
-        r, _ = test(env, agent, deterministic=False)
+    for _ in range(100):
+        r = test(env, agent, deterministic=True)
         evals.append(r)
     print(f"avg: {np.mean(evals)} max: {max(evals)} min: {min(evals)}")
+    
+    # stochastic policy test
+    #agent.action_std = 0.1
+    #print("stochastic policy test")
+    
+    #evals = []
+    #for _ in range(10):
+    #    r = test(env, agent, deterministic=False)
+    #    evals.append(r)
+    #print(f"avg: {np.mean(evals)} max: {max(evals)} min: {min(evals)}")
 
 
     env.close()
         
-
-
 
 # Main entry point
 if __name__ == "__main__":
